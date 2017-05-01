e4a436ef6058a47034f49a2f529eba1d7c816aaf
Débora Andrade <teredeby@gmail.com>
2017-04-30 21:03:33 -0300

    debugando

--- code/Classes/TelegramConnect.php
+++ code/Classes/TelegramConnect.php
@@ -4,7 +4,9 @@ class TelegramConnect
 {
     /*TODO make methods non-static, implement constructor*/
     public static function sendMessage ($chatId, $message) {
+		echo $message;
         $url = Config::getBotConfig("ApiRequestUrl")."/sendMessage?chat_id=".$chatId."&text=".urlencode($message)."&parse_mode=HTML";
+		echo $url;
         file_get_contents($url);
     }
 }
