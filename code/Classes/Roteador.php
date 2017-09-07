f8cd788b988e0579c18735d439971da14a971090
Débora Andrade <teredeby@gmail.com>
2017-09-07 20:42:08 -0300

    debugando

--- code/Classes/Roteador.php
+++ code/Classes/Roteador.php
@@ -126,7 +126,11 @@
 			$msg  = "Erro de parametros:\n";
 			$msg .=  "Uso: /" . $m ." [horario] [vagas] [local] \nEx: /";
 			$msg .= $m . " as " . $h . " 2 bb";
-			if (count($args) == 1) {
+			
+			if (count($args) == 2) {
+				error_log($args[0]);
+				error_log($args[1]);
+				error_log($args[2]);
 				$a = self::isValidTime($args[1]);
 				if ( $a[0] ){
 					$dao->createCarpool($chat_id, $user_id, $username, 
@@ -134,7 +138,12 @@
 					$msg = "@" . $username . " oferece carona de " . $m;
 					$msg .= " as " . $travel_hour;
 				}
-			}elseif (count($args) >= 3) {
+			}elseif (count($args) == 4) {
+				error_log($args[0]);
+				error_log($args[1]);
+				error_log($args[2]);
+				error_log($args[3]);
+				error_log($args[4]);
 				$a = self::isValidTime($args[1]);
 				if ( $a[0] && (gettype($args[2]) == 'integer')){
 					$spots = $args[2];
@@ -164,7 +173,7 @@
 			$msg  = "Erro de parametros:\n";
 			$msg .=  "Uso: /vagas " . $m . " [ ida | volta]  vagas \nEx: ";
 			$msg .= "/vagas " . $m . " 2";
-			if (count($args) == 2) {
+			if (count($args) == 3) {
 				$spots = $args[2];
 				$dao->updateSpots($chat_id, $user_id, $spots, $flag);
 				$msg = "@".$username." atualizou o número de vagas de ";
@@ -184,7 +193,7 @@
 			}
 			$msg  = "Erro de parametros:\n";
 			$msg .=  "Uso: /remover [ida | volta] \nEx: /remover " . $m;
-			if (count($args) == 1) {
+			if (count($args) == 2) {
 				$dao->removeCarpool($chat_id, $user_id, $flag);
 				$msg = "@".$username." removeu sua ". $m;
 			} 
